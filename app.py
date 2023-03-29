b74de601ca639592fb376bb3ad1080cf32725e92
Lazaro03 <95556479+Lazaro03@users.noreply.github.com>
2023-03-29 19:27:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return '𝐇𝐨𝐥𝐚, 𝐃𝐞 𝐀𝐧𝐭𝐞𝐦𝐚𝐧𝐨 𝐆𝐫𝐚𝐜𝐢𝐚𝐬 𝐩𝐨𝐫 𝐌𝐚𝐧𝐭𝐞𝐫 𝐌𝐢 𝐒𝐞𝐫𝐯𝐢𝐜𝐢𝐨 𝐀𝐜𝐭𝐢𝐯𝐨 😊 𝐄𝐫𝐞𝐬 𝐍𝐮𝐞𝐬𝐭𝐫𝐚 𝐑𝐚𝐳𝐨́𝐧 𝐃𝐞 𝐒𝐞𝐫'
 
-os.system("git clone https://github.com/Lazaro03/nuevobott oke && cd oke && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://github.com/Lazaro03/nuevobott.git oke && cd oke && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
